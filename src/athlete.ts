9fd84c76bf9b7cfccc005aaf000001f1795b2398
Scott González <scott.gonzalez@gmail.com>
2024-06-15 18:33:33 -0400

    ...

--- src/athlete.ts
+++ src/athlete.ts
@@ -4,19 +4,28 @@ export interface AthleteState {
     category: string;
     clean1State: AttemptState;
     clean1Weight: number | null;
+    clean1WeightPounds: number | null;
     clean2State: AttemptState;
     clean2Weight: number | null;
+    clean2WeightPounds: number | null;
     clean3State: AttemptState;
     clean3Weight: number | null;
+    clean3WeightPounds: number | null;
     cleanRank: number | null;
+    membership: string | null;
+    meta: object | null;
     name: string;
     nextWeight: number | null;
+    nextWeightPounds: number | null;
     snatch1State: AttemptState;
     snatch1Weight: number | null;
+    snatch1WeightPounds: number | null;
     snatch2State: AttemptState;
     snatch2Weight: number | null;
+    snatch2WeightPounds: number | null;
     snatch3State: AttemptState;
     snatch3Weight: number | null;
+    snatch3WeightPounds: number | null;
     snatchRank: number | null;
     startNumber: number;
     team: string;
@@ -30,6 +39,8 @@ type AttemptState =
     | 'good'
     | 'request';
 
+export type Meta = Record<string, object>;
+
 export interface OwlcmsAthlete {
     bestCleanJerk: string;
     bestSnatch: string;
@@ -41,6 +52,7 @@ export interface OwlcmsAthlete {
     flagURL: string;
     fullName: string;
     group: string;
+    membership: string;
     sattempts: OwlcmsAttempt[];
     sinclair: string;
     sinclairRank: string;
@@ -65,7 +77,13 @@ export interface OwlcmsAttempt {
 }
 
 export default class Athlete {
+    private static meta: Meta = {};
+
     private state!: AthleteState;
+
+    public static loadMeta(meta: Meta) {
+        this.meta = meta;
+    }
 
     public constructor(data: OwlcmsAthlete) {
         this.update(data);
@@ -82,12 +100,22 @@ export default class Athlete {
             : null;
     }
 
+    private getNextWeightPounds(data: OwlcmsAthlete): number | null {
+        const kilos = this.getNextWeight(data);
+        return kilos === null ? kilos : Math.round(kilos * 2.204);
+    }
+
     public getState(): AthleteState {
         return this.state;
     }
 
     private parseWeight(weight: string): number {
         return parseInt(weight.replace(/\D/g, ''));
+    }
+
+    private parseWeightPounds(weight: string): number {
+        const kilos = this.parseWeight(weight);
+        return Number.isNaN(kilos) ? kilos : Math.round(kilos * 2.204);
     }
 
     public update(data: OwlcmsAthlete): void {
@@ -97,19 +125,28 @@ export default class Athlete {
             cleanRank: parseInt(data.cleanJerkRank) || null,
             clean1State: data.cattempts[0].liftStatus,
             clean1Weight: this.parseWeight(data.cattempts[0].stringValue) || null,
+            clean1WeightPounds: this.parseWeightPounds(data.cattempts[0].stringValue) || null,
             clean2State: data.cattempts[1].liftStatus,
             clean2Weight: this.parseWeight(data.cattempts[1].stringValue) || null,
+            clean2WeightPounds: this.parseWeightPounds(data.cattempts[1].stringValue) || null,
             clean3State: data.cattempts[2].liftStatus,
             clean3Weight: this.parseWeight(data.cattempts[2].stringValue) || null,
+            clean3WeightPounds: this.parseWeightPounds(data.cattempts[2].stringValue) || null,
             category: data.category,
+            membership: data.membership || null,
+            meta: Athlete.meta[data.membership] || null,
             name: data.fullName,
             nextWeight: this.getNextWeight(data),
+            nextWeightPounds: this.getNextWeightPounds(data),
             snatch1State: data.sattempts[0].liftStatus,
             snatch1Weight: this.parseWeight(data.sattempts[0].stringValue) || null,
+            snatch1WeightPounds: this.parseWeightPounds(data.sattempts[0].stringValue) || null,
             snatch2State: data.sattempts[1].liftStatus,
             snatch2Weight: this.parseWeight(data.sattempts[1].stringValue) || null,
+            snatch2WeightPounds: this.parseWeightPounds(data.sattempts[1].stringValue) || null,
             snatch3State: data.sattempts[2].liftStatus,
             snatch3Weight: this.parseWeight(data.sattempts[2].stringValue) || null,
+            snatch3WeightPounds: this.parseWeightPounds(data.sattempts[2].stringValue) || null,
             snatchRank: parseInt(data.snatchRank) || null,
             startNumber: parseInt(data.startNumber),
             team: data.teamName,
